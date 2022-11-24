5c2380e6df0764ec82e571e5978fa17c5b71bebc
orbitusii <dlodholm@icloud.com>
2022-11-24 11:48:56 -0700

    Extract Icon, Line, Poly Styles to their own files

---
+++ Styles/LineStyle.cs
@@ -0,0 +1,24 @@
+﻿namespace O2Kml.Styles
+{
+    public class LineStyle : PolyStyle
+    {
+        public float width { get; set; } = 1;
+
+        public bool Equals (LineStyle? obj)
+        {
+            return color == obj?.color
+                && width == obj?.width;
+        }
+
+        public override string ToString()
+        {
+            return $"LineStyle: {color} @{width}px";
+        }
+
+        public LineStyle ()
+        {
+            color = "ffffffff";
+            width = 1;
+        }
+    }
+}
---
+++ Styles/PolyStyle.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace O2Kml.Styles
+{
+    public class PolyStyle
+    {
+        [XmlElement]
+        public string color { get; set; } = "84ffffff";
+
+        public bool Equals (PolyStyle? obj)
+        {
+            return color == obj?.color;
+        }
+
+        public override string ToString()
+        {
+            return $"PolyStyle: {color}";
+        }
+    }
+}
